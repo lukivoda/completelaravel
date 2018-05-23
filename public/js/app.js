7f2bf987f7d4c537085a789dbfff180f291aea73
lukivoda <stevanris@gmail.com>
2018-05-23 17:43:56 +0200

    update1

--- public/js/app.js
+++ public/js/app.js
@@ -44266,7 +44266,7 @@ $(document).ready(function () {
 /* 48 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open 'C:\\wamp64\\www\\completelaravel\\node_modules\\admin-lte\\dist\\css\\adminlte.css'\n    at Error (native)\n    at runLoaders (C:\\wamp64\\www\\completelaravel\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at C:\\wamp64\\www\\completelaravel\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\wamp64\\www\\completelaravel\\node_modules\\loader-runner\\lib\\LoaderRunner.js:200:19\n    at C:\\wamp64\\www\\completelaravel\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:70:14\n    at _combinedTickCallback (internal/process/next_tick.js:73:7)\n    at process._tickCallback (internal/process/next_tick.js:104:9)");
 
 /***/ })
 /******/ ]);
